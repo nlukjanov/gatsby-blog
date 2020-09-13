475024d0b47236d577d6c22dc28b795157871fe9
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-13 09:12:37 +0100

    add remark transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/src/markdowns`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
